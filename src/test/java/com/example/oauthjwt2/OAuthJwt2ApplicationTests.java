1cbba31bb25b92c255f38c6a7df12bf519272eaf
전재연 <sunrox1107@gmail.com>
2024-09-09 16:57:17 +0900

    first commit

---
+++ src/test/java/com/example/oauthjwt2/OAuthJwt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthjwt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthJwt2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
